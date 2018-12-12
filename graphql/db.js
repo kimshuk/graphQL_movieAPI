563fb2a577c25c013b4e670dd7684271600120d0
developer <jeesoo2002@gmail.com>
2018-12-12 13:59:28 -0800

    mutation added, ch 9

--- graphql/db.js
+++ graphql/db.js
@@ -1,6 +1,6 @@
-const movies = [
+let movies = [
   {
-    id: "0",
+    id: 0,
     name: "Star Wars - The Last Jedi",
     score: 30
   },
@@ -31,19 +31,29 @@ const movies = [
   }
 ];
 
-export const getMovies = () => getMovies;
+export const getMovies = () => movies;
 
 export const getById = id => {
-  const filteredPeople = people.filter(person => id === person.id);
-  return filteredPeople[0];
+  const filteredMovies = movies.filter(movie => id === movie.id);
+  return filteredMovies[0];
 };
 
 export const deleteMovie = id => {
   const cleanedMovies = movies.filter(movie => movie.id !== id);
   if (movies.length > cleanedMovies.length) {
-    movie = cleanedMovies;
+    movies = cleanedMovies;
     return true;
   } else {
     return false;
   }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
