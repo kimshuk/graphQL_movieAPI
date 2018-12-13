81a99acd00b19590f4e3492b2969e5a8bdc849c2
developer <jeesoo2002@gmail.com>
2018-12-12 16:59:56 -0800

    push before go

--- graphql/db.js
+++ graphql/db.js
@@ -7,28 +7,3 @@ export const getMovies = (limit, rating) => {
     .then(res => res.json())
     .then(json => json.data.movies);
 };
-
-// export const getById = id => {
-//   const filteredMovies = movies.filter(movie => id === movie.id);
-//   return filteredMovies[0];
-// };
-
-// export const deleteMovie = id => {
-//   const cleanedMovies = movies.filter(movie => movie.id !== id);
-//   if (movies.length > cleanedMovies.length) {
-//     movies = cleanedMovies;
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-
-// export const addMovie = (name, score) => {
-//   const newMovie = {
-//     id: `${movies.length + 1}`,
-//     name,
-//     score
-//   };
-//   movies.push(newMovie);
-//   return newMovie;
-// };
